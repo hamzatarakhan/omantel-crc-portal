f429e4ba3d6662c54cda5c1df83780f653804665
Hamza Tarkan <hamza.tarkan@leading-point.com>
2026-09-17 23:56:08 +0300

    Auto-generated commit

---
+++ chunk-O4ZSTVKY.js
@@ -0,0 +1,1 @@
+import{z as o}from"./chunk-FSY2KVIK.js";import{J as t,ra as i}from"./chunk-JROZ5OPK.js";var m=(()=>{class e{static \u0275fac=function(r){return new(r||e)};static \u0275mod=i({type:e});static \u0275inj=t({imports:[o,o]})}return e})();export{m as a};
